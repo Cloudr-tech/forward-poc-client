9fc26b893093803a941f8465c76e8a36c1e04bcd
Pierre-Alexandre Monneau <pierre-alexandre.monneau@epitech.eu>
2018-03-15 11:39:41 +0100

    files container

--- src/components/FileItem.js
+++ src/components/FileItem.js
@@ -3,12 +3,33 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import 'font-awesome/css/font-awesome.min.css';
+
+const style = {
+    rightColumn: {
+      textAlign: 'right',
+      color: 'black',
+      fontSize: '18px',
+      paddingRight: '0',
+    },
+    firstColumn: {
+      width: '60px',
+      marginLeft: 0,
+      paddingLeft: 0,
+      paddingRight: 0,
+      fontSize: '18px',      
+    },
+    secondColumn: {
+      fontSize: '18px',
+      paddingLeft: 0,
+    }
+};
 
 const FileItem = props => (
   <TableRow>
-    <TableRowColumn>{props.id}</TableRowColumn>
-    <TableRowColumn>{props.name}</TableRowColumn>
-    <TableRowColumn>{props.size}</TableRowColumn>
+    <TableRowColumn style={style.firstColumn}>{props.id}</TableRowColumn>
+    <TableRowColumn style={style.secondColumn}>{props.name}</TableRowColumn>
+    <TableRowColumn style={style.rightColumn}>{props.size} <i className='fa fa-download'/></TableRowColumn>
   </TableRow>
 );
 
--- src/views/Files/Files.js
+++ src/views/Files/Files.js
@@ -6,6 +6,16 @@ import {
 } from 'material-ui/Table';
 import './Files.css';
 
+const style = {
+  Table: {
+    width: '95%',
+    marginTop: '50px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontSize: '16px',
+  }
+}
+
 class Files extends Component {
   constructor() {
     super();
@@ -14,9 +24,10 @@ class Files extends Component {
   render() {
     return (
       <div className='FilesContainer'>
-        <div className='FilesHeader'></div>
-        <h1 className='FilesTitle'>Files</h1>
-        <Table>
+        <div className='FilesHeader'>
+          <h3>Tous les fichiers</h3>
+        </div>
+        <Table style={style.Table}>
           <TableBody>
             {this.props.files.map((file) => (
               <FileItem
